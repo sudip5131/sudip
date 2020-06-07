169518461cc4f3ec16c0e26e712b2a09b892fb0e
Sudip Maharjan <messudip19@gmail.com>
2020-06-07 09:40:01 +0545

    yo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarlinks = document.getElementsByClassName("navbar-links")[0];
+
+toggleButton.addEventListener("click", () => {
+  navbarlinks.classList.toggle("active");
+});
 jQuery(document).ready(function () {
   var ua = navigator.userAgent.toLowerCase();
   if (
